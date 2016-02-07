0faebaaae11661d792c006e60e4e7653a2c5129b
Roger Rodriguez <ICreate@Almightys-MacBook-Air.local>
2016-02-07 18:06:01 -0500

    real

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,13 @@ ActiveRecord::Schema.define(version: 20160206203713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "resources", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
