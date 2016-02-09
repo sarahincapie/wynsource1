d635ebb1f427faf7e8e770aae8cc37bec816ca71
sarahincapie <saral85@gmail.com>
2016-02-08 19:14:40 -0500

    took off description from index

--- app/assets/javascripts/markdown.js
+++ app/assets/javascripts/markdown.js
@@ -1,6 +1,5 @@
 $(document).on("ready page:load", function() {
 	$(".markdown").each(function(i) {
-		console.log(marked($( this ).html()));
     	$( this ).html(marked($( this ).html()));
   });
 });
